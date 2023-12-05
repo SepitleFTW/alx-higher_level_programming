6dc88a4fc171b5c171b129235214084fcbbd4ecf
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2023-12-05 16:37:40 +0200

    ...

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 1           # a is assigned the value of 1
+    b = 2           #b is assigned the value of 2
+
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
