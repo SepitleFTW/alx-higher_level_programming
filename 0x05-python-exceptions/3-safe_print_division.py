5c27aab3bf7b2bee3b48b2e86ef3f81850d590a4
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2023-12-19 20:59:54 +0200

    ...

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         div = a / b
 
-    except (TypeError, ZerroDivisionError):
+    except (TypeError, ZeroDivisionError):
         div = None
 
     finally:
