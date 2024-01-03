9c951799b8f6ed3b9eb69df1859323700b548091
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-03 18:16:50 +0200

    big things have samll beginnings'

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,9 +15,9 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     if type(a) is float:
-        a = int (a)
+        a = int(a)
 
     if type(b) is float:
-        b == int (b)
+        b == int(b)
 
     return a + b
