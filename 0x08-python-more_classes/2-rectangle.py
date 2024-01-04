141b95f9b464fb23ab7f94b3d38e4ceb5218c16a
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-04 19:51:59 +0200

    baby steps

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,14 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin./python3
 """
 class that defines a rectangle
 """
-
 class Rectangle:
-    """init the width and height
-    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
         return self.__width
@@ -31,4 +29,14 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = height
+
+    def area(self):
+        return self.__area
+
+    def perimeter(self):
+        return self.__perimeter
+
+        if self.width == 0 or self.height == 0:
+            return perimeter
+
