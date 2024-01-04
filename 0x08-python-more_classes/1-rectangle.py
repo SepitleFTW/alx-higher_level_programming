4fc1da7ef745310029bb7264f00559b970acaf65
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-04 19:28:25 +0200

    baby steps

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,8 +19,9 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
 
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
+        slef.__width = value
 
     @property
     def height(self, value):
@@ -31,7 +32,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
 
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
 
         self.__height = value
