90df45facb858402e7e0c09cc2bbd847b433c73b
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-04 19:31:04 +0200

    baby steps

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
     """init the width and height
     """
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -18,7 +18,6 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-
         elif value < 0:
             raise ValueError("width must be >= 0")
         slef.__width = value
@@ -31,9 +30,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-
         elif value < 0:
             raise ValueError("height must be >= 0")
-
         self.__height = value
 
