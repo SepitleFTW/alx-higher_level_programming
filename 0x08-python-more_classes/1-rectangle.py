62f84ed8e924c6cb49136fa471969e9524d0abfd
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-04 19:32:31 +0200

    baby steps

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        slef.__width = value
+        self.__width = value
 
     @property
     def height(self, value):
