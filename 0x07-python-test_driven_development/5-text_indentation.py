682314ad340ebe84d42bf5a7a8d4088b8c730b39
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-08 10:33:55 +0200

    ...

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+""" text indentation func"""
 
 def text_indentation(text):
+    """ new line starts after '.' '?' and ':'
+    """
     if not isinstance(text, string):
         raise TypeError("text must be a string")
 
 def print_text(text):
+    """ prints new text """
     for char in text:
         print(char, end="")
 
         if char in ".?:":
             print("\n\n")
 
-print("Hey. I am a noob")
+
 
