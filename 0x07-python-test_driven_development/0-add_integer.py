bfe7eabe737e865b4add87fa3648a4f00840b700
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-08 10:21:37 +0200

    ...

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,16 +6,16 @@ def add_integer(a, b=98):
     """ adds 2 numbers
     """
 
-    if ((not isinstance(type(a), int))):
+    if ((not isinstance(a, int))):
        raise TypeError("a must be an integer")
 
-    if ((not isinstance(type(a), float))):
+    if ((not isinstance(a, float))):
         raise TypeError("a must be an integer")
 
-    if ((not isinstance(type(b), int))):
+    if ((not isinstance(b, int))):
         raise TypeError("b ust be an integer")
 
-    if ((not isinstance(type(b), float))):
+    if ((not isinstance(b, float))):
         raise TypeError("b must be an integer")
 
     return (int(a) + int(b))
