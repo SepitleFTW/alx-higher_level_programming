1a87086b6ec4458e4ef5ad8e7a919c4617979eee
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-08 10:19:30 +0200

    ...

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,17 +3,19 @@
 """
 
 def add_integer(a, b=98):
-    """ adds 2 numbers """
-    if not isinstance(type(a), int):
+    """ adds 2 numbers
+    """
+
+    if ((not isinstance(type(a), int))):
        raise TypeError("a must be an integer")
 
-    if not isinstance(type(a), float):
+    if ((not isinstance(type(a), float))):
         raise TypeError("a must be an integer")
 
-    if not isinstance(type(b), int):
+    if ((not isinstance(type(b), int))):
         raise TypeError("b ust be an integer")
 
-    if not isinstance(type(b), float):
+    if ((not isinstance(type(b), float))):
         raise TypeError("b must be an integer")
 
     if type(a) is float:
@@ -22,4 +24,4 @@ def add_integer(a, b=98):
     if type(b) is float:
         b = int(b)
 
-    return (a + b)
+    return (int(a) + int(b))
