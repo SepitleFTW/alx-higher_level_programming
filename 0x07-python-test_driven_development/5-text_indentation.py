a9ca3eb82e7a042b73f9338140d6f22fb564d7c4
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-08 10:39:22 +0200

    ...

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,13 +7,17 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-def print_text(text):
-    """ prints new text """
-    for char in text:
-        print(char, end="")
+    s = 0
+    while s < len(text) and text[s] == ".?:":
+        s + =1
 
-        if char in ".?:":
-            print("\n\n")
-
-
-
+    while s < len(text):
+        print(text[s], end="")
+        if text[s] == "\n" or text[s] in ".?:":
+            if text[s] in ".:?":
+                print("\n")
+            s += 1
+            while s < len(text) and text[s] == ' ':
+                s += 1
+                continue
+            c += 1
