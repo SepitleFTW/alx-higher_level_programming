e116fa8fbf6f6f936533e4c4b25c1e65d6406c18
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-08 10:28:38 +0200

    ...

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,10 +6,10 @@ def say_my_name(first_name, last_name=""):
     """
     prints a name
     """
-    if not isinstance(first_name, string):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    if not isinstance(last_name, string):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
 
