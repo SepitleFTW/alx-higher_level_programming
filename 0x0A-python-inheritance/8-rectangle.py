28b3b878321487791d83137f7ffb110383183c13
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-09 22:23:38 +0200

    ...

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """rectangle that inherits from basegeo"""
+BaseGeometry = ___import___('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ rectangle class"""
