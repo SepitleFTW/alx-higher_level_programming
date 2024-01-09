e87c73c1e3765a8bf0edf0bf80cdc7ee8a0db41b
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-09 21:40:16 +0200

    ...

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""functioclass else return false"""
+
+def is_same_class(obj, a_class):
+    """ insert comment I very sleepy lmao"""
+   if type(obj) == a_class:
+       return True
+   return False
