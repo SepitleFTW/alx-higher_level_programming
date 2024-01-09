dccffd3bf0f919798d24810c847a2143fbc12376
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-09 22:03:42 +0200

    ...

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" write basegeometry based on question 6"""
+
+class BaseGeometry:
+    """empty class"""
+
+    def area(self):
+        """ raise the below typerror"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """init and set values"""
+        self.name = name
+        self.value = value
+
+        if not isinstance(value, int):
+            raise("<name> must be an integer")
+
+        if value =< 0:
+            raise ValueError("<name> must be greater than 0")
+
