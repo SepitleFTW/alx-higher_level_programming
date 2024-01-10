9bf04292c7681d5c9e7df5ab3826103a5353cf56
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 19:35:21 +0200

    ...

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""write an object to a text file using json representation"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Saves an object to a JSON file."""
+
+    with open(openfile, 'w'):
+        json.dump(my_obj, openfile)
