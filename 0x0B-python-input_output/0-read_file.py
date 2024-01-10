6470ebe8561e0a894754e40913d9d06f80d4aa94
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 18:18:51 +0200

    ...

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    with open(UTF8, "r") as f:
+        return f.read()
---
+++ 0x0B-python-input_output/tests/input.py
@@ -0,0 +1,5 @@
+f = open('workfile', 'w', encoding='utf-8')
+
+with open('workfile', encoding='utf-8') as f:
+
+    f.read()
