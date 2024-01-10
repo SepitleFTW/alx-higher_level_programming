5ac5326dc47ea34667880ba1a9de456e8635f9ec
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 19:20:46 +0200

    ...

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a func that returns JSON repr of a string"""
+
+
+def to_json_string(my_obj):
+    """Returns a JSON string representation of an object."""
+
+    return json.dumps(my_obj)
