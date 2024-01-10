48079519ccf22cefd1658c21b5f67f6299d76088
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 18:53:54 +0200

    ...

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,6 @@
 """ a function writing string to utf8 and
 returns chars when written"""
 
-
 def write_file(filename="", text=""):
     """ write text to file"""
     with open(filename, "w", encoding="utf-8") as f:
