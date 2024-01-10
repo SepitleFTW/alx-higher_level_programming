80193509b3a0c57e66e6dd971ef324c7b611469f
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 20:17:35 +0200

    input output

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""reads stdin line by line and compute metrics"""
+import sys
+from collections import defaultdict
+
+total_size = 0
+status_counts = defaultdict(int)
+line_count = 0
+
+try:
+
+    for line in sys.stdin:
+        """insert comment here later"""
+        parts = line.strip().split()
+        size = int(parts[-1])
+        status_code = parts[-2]
+
+        total_size += size
+
+        status_counts[status_code] += 1
+
+        line_count += 1
+
+        if line_count % 10 == 0:
+            print("Total file size:", total_size)
+            for status_code in sorted(status_counts.keys()):
+                count = status_counts[status_code]
+                print(status_code + ":", count)
+
+    print("Total file size:", total_size)
+    for status_code in sorted(status_counts.keys()):
+        count = status_counts[status_code]
+        print(status_code + ":", count)
+
+except KeyboardInterrupt:
+
+    print("\nTotal file size:", total_size)
+    for status_code in sorted(status_counts.keys()):
+        count = status_counts[status_code]
+        print(status_code + ":", count)
