1827ac80ef7ded26b87a9fbb79ec47146dcc7ad3
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 18:20:08 +0200

    input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+    """Read a UTF-8 encoded file."""
     with open(UTF8, "r") as f:
         return f.read()
--- 0x0B-python-input_output/tests/input.py
+++
@@ -1,5 +0,0 @@
-f = open('workfile', 'w', encoding='utf-8')
-
-with open('workfile', encoding='utf-8') as f:
-
-    f.read()
