90f90044d255243ed79d249816755816eadb0766
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 19:36:13 +0200

    ...

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Saves an object to a JSON file."""
-
-    with open(openfile, 'w'):
-        json.dump(my_obj, openfile)
+    with open(filename, 'w') as outfile:
+        json.dump(my_obj, outfile)
