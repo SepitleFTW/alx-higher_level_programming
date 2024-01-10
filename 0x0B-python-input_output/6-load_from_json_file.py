31441996bbb23721c98a9c3d47ba7b37d22aebae
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 20:01:00 +0200

    ..

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """loads an obj from JSON file"""
-    with open(filename) as json file:
-        return json.load(json_file)
+    with open(filename) as f:
+        return json.load(f)
