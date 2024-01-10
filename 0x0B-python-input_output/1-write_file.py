bb08571d786a8a459cbd845c2e62fe0d3ac19006
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-10 18:57:15 +0200

    input output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,9 @@
 
 
 def write_file(filename="", text=""):
-    """ write text to file"""
+    """ write text to file
+    filename (str): file name
+    text (str): text to write"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
 
