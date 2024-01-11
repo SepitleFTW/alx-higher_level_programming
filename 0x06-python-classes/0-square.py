fe4d27a5a701af59541c30f6760ed0fc19caaa9e
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-11 09:04:39 +0200

    ...

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"""empty class that defines a square"""
 
 
 class Square:
-    """Represent a square."""
+    """empty class that defines a square"""
     pass
