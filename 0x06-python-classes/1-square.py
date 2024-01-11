5b18b0fd605a7856028c3fb5722f3ce2b39aa922
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-11 09:09:23 +0200

    ...

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""class thhat defines a sqiare based on prev question"""
-
-
 class Square:
-    """ class for square"""
+
+
     def __init__(self, size):
+
+
         self.size = size
