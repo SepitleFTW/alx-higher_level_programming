a97ea41c77caa00d8c8ca3a43d4dce3a065eb8dd
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-15 22:36:18 +0200

    ...88

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- script that grants privileges.
 -- of users_0d_1 and 2.
-SELECT *
-FROM mysql.user
-WHERE user IN ('user_0d_1', 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
