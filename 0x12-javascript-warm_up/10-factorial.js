65cade1aec33e888635bb103268a72eceb1d3028
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-03-11 15:46:40 +0200

    ...

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
+
 function factorial (n) {
-  if (n < 0) {
-    return (-1);
+  if (isNaN(n)) {
+    return 1;
+  } else if (n < 0) {
+    return 'Factorial is not defined for negative numbers';
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
   }
-  if (n === 0 || isNaN(n)) {
-    return (1);
-  }
-  return (n * factorial(n - 1));
 }
 
-console.log(factorial(Number(process.argv[2])));
+const input = parseInt(process.argv[2]);
+console.log(factorial(input));
