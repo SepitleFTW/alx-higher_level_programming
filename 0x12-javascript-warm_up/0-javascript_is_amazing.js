8e69e20bfc3912c16fb815fb479c12dce8073976
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-03-11 12:47:37 +0200

    javascriupt is like c

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-/* a script that will run Javascript is amazing
-*/
-
 const myVar = "Javascript is amazing";
 console.log(myVar);
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-/*
-this script will print the first argument passed to it
- */
-
 const arg = process.argv[2];
 
 if (arg === undefined) {
