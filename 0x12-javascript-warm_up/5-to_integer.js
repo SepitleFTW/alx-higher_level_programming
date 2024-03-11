f7c51663eccdc34f63b93ec153851a595d66cbaf
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-03-11 14:04:45 +0200

    ...

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] === Number()) {
-    console.log('My number:');
+if (!isNaN(process.argv[2])) {
+    console.log('My number:', process.argv[2]);
 } else {
     console.log('Not a number');
 }
-
