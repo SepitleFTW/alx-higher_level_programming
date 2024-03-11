38671ad6ebd1fafb0587249c6a6a66f9bdb84db2
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-03-11 15:17:56 +0200

    ss

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const x = parseIn(process.argv[2]);
+if (!isNaN(x)) {
+  for (let i = 0; i < x.length; i++) {
+  console.log('C is fun');
+}
+} else {
+  console.log('Missing number of occurences');
+}
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const square = parseInt(process.argv[2]);
+if (!isNaN(square)) {
+  if (square > 0) {
+  for (let i = 0; i < square; i++) {
+  console.log('X'.repeat(square));
+  }
+} else {
+  console.log('Missing size');
+}
+} else {
+    console.log('Missing size');
+}
