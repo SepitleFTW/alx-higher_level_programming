fa55303653c28c56bfbb07fd76ab27f39f418ce7
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-03-11 15:05:02 +0200

    ..

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const lines = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
-
 for (let ii = 0; ii < lines.length; ii++) {
   console.log(lines[ii]);
 }
