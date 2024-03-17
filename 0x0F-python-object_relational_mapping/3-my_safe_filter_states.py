b64ec604c6b7ca79c7aaacd9cee942c8bf8fa6f2
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-03-17 13:05:19 +0200

    ...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""lists all the states from the database"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-if__name__ == '__main__':
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
@@ -15,4 +15,3 @@ if__name__ == '__main__':
         print(row)
     cur.close()
     db.close()
-
