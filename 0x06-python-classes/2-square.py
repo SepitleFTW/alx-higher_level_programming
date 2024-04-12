388eaad37eef41b97c64b32c21cf422dbca8ab0a
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-04-12 16:31:26 +0200

    square class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""
+a class square that defines a square
+"""
+
 
 class Square:
-
-
+    """
+    a class square that defines a square
+    """
     def __init__(self, size=0):
+        self.__size = size
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        if size < 0:
+            raise ValueError("size must be a positive integer")
