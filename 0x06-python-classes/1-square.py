fc46c54c96d83a6acae0757a8c2ea2ae1bfb77d7
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-04-12 16:25:38 +0200

    ..

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""
+a class square that defines a square
+"""
 
+class Square:
+    """
+    a class square that defines a square
+    """
+    def __init__(self, size):
+        self.__size = size
